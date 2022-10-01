e80c265d3367ab5fa469fee92bffd5ba756e59e5
Mofejesu Famuyiwa <niciepearl@gmail.com>
2022-10-01 20:34:38 +0100

    done with calculator index.html

---
+++ script.js
@@ -0,0 +1,91 @@
+let output = document.getElementById('output');
+
+function zero() {
+    output.value += 0
+} 
+
+function one() {
+    output.value += 1
+}
+
+function two() {
+    output.value += 2
+}
+
+function three() {
+    output.value += 3
+}
+
+function four() {
+    output.value += 4
+}
+
+function five() {
+    output.value += 5
+}
+
+function six() {
+    output.value += 6
+}
+
+function seven() {
+    output.value += 7
+}
+
+function eight() {
+    output.value += 8
+}
+
+function nine() {
+    output.value += 9
+}
+
+function ac() {
+    output.value += "ac"
+} 
+
+function bracketopen() {
+    output.value += "("
+}
+
+function bracketclose() {
+    ouput.value += ")"
+}
+
+function negation() {
+    output.value += "~"
+}
+
+function divide() {
+    output.value += "/"
+}
+
+function multiply() {
+    output.value += "*"
+}
+
+function add() {
+    output.value += "+"
+}
+
+function modulus() {
+    output.value += "%"
+}
+
+function subtract() {
+    output.value += "-"
+}
+
+function decimal() {
+    output.value += "."
+}
+
+function deletesign() {
+    output.value += "del"
+}
+
+//function equalsign() {
+   // let output = document.getElementById("output");
+   // let result = eval(output.value); 
+  //  output.value += "="
+//}
